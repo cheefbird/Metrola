ce3e60db3ab4b036bdbb889652b248857247c857
cheefbird <temp@mail.com>
2024-07-14 18:51:12 -0700

    simple Line and Direction structs

---
+++ Metrola/Model/Direction.swift
@@ -0,0 +1,16 @@
+//
+//  Direction.swift
+//  Metrola
+//
+//  Created by Francis Breidenbach on 7/14/24.
+//
+
+import Foundation
+
+struct Direction {
+    var id: String
+    var title: String
+    var stops = [Station]()
+    var headsigns: [String]
+    
+}
--- Metrola/Model/Line.swift
+++ Metrola/Model/Line.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+/// Note: Appears as "route" from source API
+struct Line {
+    var id: String
+    var name: String
+    var color: String
+    var type: String
+    var directions = [String: Any]()
+}
