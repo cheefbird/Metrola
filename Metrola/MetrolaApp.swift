2b6fc1b07b9ba74e8e2d6505143a640f0cd772b7
Francis Breidenbach <temp@mail.com>
2024-07-19 16:21:06 -0700

    attach modelContainer for Line

--- Metrola/MetrolaApp.swift
+++ Metrola/MetrolaApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct MetrolaApp: App {
@@ -13,5 +14,6 @@ struct MetrolaApp: App {
         WindowGroup {
             RootView()
         }
+        .modelContainer(for: Line.self)
     }
 }
