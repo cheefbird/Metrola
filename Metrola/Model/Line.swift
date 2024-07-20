e493f0122a8ae65a06574b162de4c4936fb85041
Francis Breidenbach <temp@mail.com>
2024-07-20 16:25:45 -0700

    methods for getting all Lines and type adjustments

--- Metrola/Model/Line.swift
+++ Metrola/Model/Line.swift
@@ -6,12 +6,22 @@
 //
 
 import Foundation
+import SwiftyJSON
 
 /// Note: Appears as "route" from source API
-struct Line: Decodable {
-    var id: String
+struct Line {
+    var id: Int
     var name: String
     var color: String
-    var type: String
+    var type: Int
     var directions = [String: String]()
+    
+    /// Create Line from JSON type.
+    init(fromJSON json: JSON) {
+        self.id = json["id"].intValue
+        self.name = json["name"].stringValue
+        let rawColor = json["color"].stringValue
+        self.color = "#\(rawColor)"
+        self.type = json["type"].intValue
+    }
 }
--- Metrola/Network/MetroNet.swift
+++ Metrola/Network/MetroNet.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 class MetroNet {
     static let shared = MetroNet()
@@ -14,15 +15,33 @@ class MetroNet {
     private init() {}
     
     // MARK: - Requests
-    func getLines() {
+    func getLines(completion: @escaping ([Line]) -> Void) {
         AF.request(MetroNetRouter.getLines)
             .validate()
             .responseData { response in
                 guard let data = response.value else {
                     fatalError()
                 }
-
+                let lines = self.processLines(from: data)
+                
+                completion(lines)
             }
     }
     
+    // MARK: - Handlers
+    private func processLines(from data: Data) -> [Line] {
+        var lines = [Line]()
+        
+        do {
+            let json = try JSON(data: data)
+            let rawLines = json["data"]["routes"].arrayValue
+            for line in rawLines {
+                lines.append(Line(fromJSON: line))
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        return lines
+    }
 }
