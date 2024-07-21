3fbf7e42d0b7442b20c341747e62ebe5cf844a9f
Francis Breidenbach <temp@mail.com>
2024-07-20 18:17:59 -0700

    add getStations(Int) route and parameter encoding for requests

--- Metrola/Network/MetroNetRouter.swift
+++ Metrola/Network/MetroNetRouter.swift
@@ -10,30 +10,42 @@ import Foundation
 
 enum MetroNetRouter: URLRequestConvertible {
     case getLines
-    
+    case getStations(Int)
+
     var baseURL: URL {
         return URL(string: "https://api.goswift.ly")!
     }
-    
+
     var method: HTTPMethod { return .get }
-    
+
     var path: String {
         return "info/lametro-rail/routes"
     }
-    
-    
-    
+
+    var parameters: [String: String] {
+        switch self {
+        case let .getStations(line):
+            return [
+                "route": "\(line)",
+                "verbose": "true",
+            ]
+        default:
+            return [:]
+        }
+    }
+
     func asURLRequest() throws -> URLRequest {
         let url = baseURL.appendingPathComponent(path)
         var request = URLRequest(url: url)
         request.method = method
         
+
         let headers: HTTPHeaders = [
             "Accept": "application/json",
-            "Authorization": Support.key.rawValue
+            "Authorization": Support.key.rawValue,
         ]
         request.headers = headers
-        
-        return request
+
+        return try URLEncodedFormParameterEncoder().encode(parameters, into: request)
     }
 }
