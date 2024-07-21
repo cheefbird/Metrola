ee988387a41757964273d7eae96e68a6602c1d3e
Francis Breidenbach <temp@mail.com>
2024-07-21 16:27:36 -0700

    implement stations array method

--- Metrola/Network/MetroNet.swift
+++ Metrola/Network/MetroNet.swift
@@ -5,16 +5,17 @@
 //  Created by Francis Breidenbach on 7/15/24.
 //
 
-import Foundation
 import Alamofire
+import Foundation
 import SwiftyJSON
 
 class MetroNet {
     static let shared = MetroNet()
-    
+
     private init() {}
-    
+
     // MARK: - Requests
+
     func getLines(completion: @escaping ([Line]) -> Void) {
         AF.request(MetroNetRouter.getLines)
             .validate()
@@ -23,11 +24,11 @@ class MetroNet {
                     fatalError("getLines response broken")
                 }
                 let lines = self.buildLinesArray(from: data)
-                
+
                 completion(lines)
             }
     }
-    
+
     func getStations(forLineID lineID: Int, completion: @escaping ([Station]) -> Void) {
         AF.request(MetroNetRouter.getStations(lineID))
             .validate()
@@ -35,16 +36,17 @@ class MetroNet {
                 guard let data = response.value else {
                     fatalError("getStations response broken")
                 }
-                let stations = self.buildStationsArray(from: data)
-                
+                let stations = self.buildStationsArray(forLineID: lineID, from: data)
+
                 completion(stations)
             }
     }
-    
+
     // MARK: - Handlers
+
     private func buildLinesArray(from data: Data) -> [Line] {
         var lines = [Line]()
-        
+
         do {
             let json = try JSON(data: data)
             let rawLines = json["data"]["routes"].arrayValue
@@ -54,13 +56,24 @@ class MetroNet {
         } catch {
             print(error.localizedDescription)
         }
-        
+
         return lines
     }
-    
-    private func buildStationsArray(from data: Data) -> [Station] {
+
+    private func buildStationsArray(forLineID lineID: Int, from data: Data) -> [Station] {
         var stations = [Station]()
-        // TODO: encoding to array
+
+        do {
+            let json = try JSON(data: data)
+            let rawStations = json["data"]["routes"].arrayValue
+                .first!["directions"].arrayValue
+                .first!["stops"].arrayValue
+            for station in rawStations {
+                stations.append(Station(forLine: lineID, fromJSON: station))
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
         return stations
     }
 }
