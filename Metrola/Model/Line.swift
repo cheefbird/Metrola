710063de8f608815a029dd5dd665f5656344aeb1
Francis Breidenbach <temp@mail.com>
2024-07-24 18:20:15 -0700

    conform to decodable

--- Metrola/Model/Line.swift
+++ Metrola/Model/Line.swift
@@ -11,12 +11,28 @@ import SwiftData
 
 /// Note: Appears as "route" from source API
 @Model
-class Line {
+class Line: Decodable {
     @Attribute(.unique) let id: Int
     let name: String
     let color: String
     let type: Int
 //    var stations = [Station]()
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case color
+        case type
+    }
+    
+    required init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(Int.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        var preColor = try container.decode(String.self, forKey: .color)
+        color = "#\(preColor)"
+        type = try container.decode(Int.self, forKey: .type)
+    }
     
     /// Create Line from JSON type.
     init(fromJSON json: JSON) {
