0551fed0de4706061014a52d42850e0d5f1bd66e
Francis Breidenbach <temp@mail.com>
2024-08-04 15:02:01 -0700

    formatting clean up

--- Metrola/Model/Line.swift
+++ Metrola/Model/Line.swift
@@ -6,46 +6,46 @@
 //
 
 import Foundation
-import SwiftyJSON
 import SwiftData
+import SwiftyJSON
 
 /// Note: Appears as "route" from source API
 @Model
 class Line: Decodable {
-    @Attribute(.unique) var id: Int
-    var name: String
-    var color: String
-    var type: Int
+  @Attribute(.unique) var id: Int
+  var name: String
+  var color: String
+  var type: Int
 //    var stations = [Station]()
-    
-    enum CodingKeys: String, CodingKey {
-        case id
-        case name
-        case color
-        case type
-    }
-    
-    required init(from decoder: any Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        id = try container.decode(Int.self, forKey: .id)
-        name = try container.decode(String.self, forKey: .name)
-        let preColor = try container.decode(String.self, forKey: .color)
-        color = "#\(preColor)"
-        type = try container.decode(Int.self, forKey: .type)
-    }
-    
-    /// Create Line from JSON type.
-    init(fromJSON json: JSON) {
-        self.id = json["id"].intValue
-        self.name = json["name"].stringValue
-        let rawColor = json["color"].stringValue
-        self.color = "#\(rawColor)"
-        self.type = json["type"].intValue
-    }
+
+  enum CodingKeys: String, CodingKey {
+    case id
+    case name
+    case color
+    case type
+  }
+
+  required init(from decoder: any Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    id = try container.decode(Int.self, forKey: .id)
+    name = try container.decode(String.self, forKey: .name)
+    let preColor = try container.decode(String.self, forKey: .color)
+    color = "#\(preColor)"
+    type = try container.decode(Int.self, forKey: .type)
+  }
+
+  /// Create Line from JSON type.
+  init(fromJSON json: JSON) {
+    id = json["id"].intValue
+    name = json["name"].stringValue
+    let rawColor = json["color"].stringValue
+    color = "#\(rawColor)"
+    type = json["type"].intValue
+  }
 }
 
 extension Line: Identifiable, Equatable {
-    static func == (lhs: Line, rhs: Line) -> Bool {
-        return lhs.id == rhs.id
-    }
+  static func == (lhs: Line, rhs: Line) -> Bool {
+    return lhs.id == rhs.id
+  }
 }
