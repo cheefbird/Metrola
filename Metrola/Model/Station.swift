d12f7c2f1c7da9234fb6a0785d0eb5b79b88f57d
Francis Breidenbach <temp@mail.com>
2024-08-04 14:58:20 -0700

    Add static example and formatting

--- Metrola/Model/Station.swift
+++ Metrola/Model/Station.swift
@@ -6,45 +6,63 @@
 //
 
 import Foundation
-import SwiftyJSON
 import SwiftData
+import SwiftyJSON
 
 @Model
 class Station: Decodable {
-    @Attribute(.unique) var id: Int
-    var name: String
-    var code: Int
-    var latitude: Double
-    var longitude: Double
-    var lineID = [Int]()
-    
-    enum CodingKeys: String, CodingKey {
-        case id
-        case name
-        case code
-        case latitude = "lat"
-        case longitude = "lon"
+  @Attribute(.unique) var id: Int
+  var name: String
+  var code: Int
+  var latitude: Double
+  var longitude: Double
+  var lineID = [Int]()
+
+  enum CodingKeys: String, CodingKey {
+    case id
+    case name
+    case code
+    case latitude = "lat"
+    case longitude = "lon"
+  }
+
+  init(id: Int, name: String, code: Int, latitude: Double, longitude: Double) {
+    self.id = id
+    self.name = name
+    self.code = code
+    self.latitude = latitude
+    self.longitude = longitude
+  }
+
+  required init(from decoder: any Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    id = try container.decode(Int.self, forKey: .id)
+    name = try container.decode(String.self, forKey: .name)
+    code = try container.decode(Int.self, forKey: .code)
+    latitude = try container.decode(Double.self, forKey: .latitude)
+    longitude = try container.decode(Double.self, forKey: .longitude)
+  }
+
+  init(forLine line: Int?, fromJSON json: JSON) {
+    id = json["id"].intValue
+    name = json["name"].stringValue
+    code = json["code"].intValue
+    latitude = json["lat"].doubleValue
+    longitude = json["lon"].doubleValue
+    if let line {
+      lineID.append(line)
     }
-    
-    required init(from decoder: any Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        id = try container.decode(Int.self, forKey: .id)
-        name = try container.decode(String.self, forKey: .name)
-        code = try container.decode(Int.self, forKey: .code)
-        latitude = try container.decode(Double.self, forKey: .latitude)
-        longitude = try container.decode(Double.self, forKey: .longitude)
-    }
-        
-    init(forLine line: Int? , fromJSON json: JSON) {
-        self.id = json["id"].intValue
-        self.name = json["name"].stringValue
-        self.code = json["code"].intValue
-        self.latitude = json["lat"].doubleValue
-        self.longitude = json["lon"].doubleValue
-        if let line {
-            self.lineID.append(line)
-        }
-    }
+  }
+
+  #if DEBUG
+    static let example = Station(
+      id: 80122,
+      name: "7th Street / Metro Center Station - Metro A & E Lines",
+      code: 80122,
+      latitude: 34.04861,
+      longitude: -118.258822
+    )
+  #endif
 }
 
-//extension Station: Identifiable, Equatable {}
+// extension Station: Identifiable, Equatable {}
